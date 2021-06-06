53359404c90843c253bad89a9809e035007e7bd1
mtsmtry <ryoi.matz@gmail.com>
2021-06-06 20:28:08 +0900

    add .tgz

--- lib/src/system/Database.ts
+++ lib/src/system/Database.ts
@@ -57,7 +57,7 @@ export function initFirestore() {
         token_uri: "https://oauth2.googleapis.com/token",
         auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
         client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-b5hjs%40exabroker-c8fed.iam.gserviceaccount.com"
-    };     
+    };
     /* 
     try {
         config = require(`../../../firestore-config.json`);
