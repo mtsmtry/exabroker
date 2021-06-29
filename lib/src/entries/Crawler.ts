4145e3fdcc3a4e169943d160e78748c5be32d8f6
mtsmtry <ryoi.matz@gmail.com>
2021-06-30 06:16:32 +0900

    add: InitCrawler

--- lib/src/entries/Crawler.ts
+++ lib/src/entries/Crawler.ts
@@ -7,33 +7,11 @@ import { getRepositories } from "../system/Database";
 import { Document } from "../system/Document";
 import { sleep } from "../Utils";
 
-const arraySplit = <T = object>(array: T[], n: number): T[][] =>
-  array.reduce((acc: T[][], c, i: number) => (i % n ? acc : [...acc, ...[array.slice(i, i + n)]]), []);
-
 async function run() {
     console.log("start");
     const reps = await getRepositories();
     const crawlingRep = reps.crawling;
     await crawlingRep.stopAllRunningTasks();
-
-    // ここで各カテゴリの1ページ目をクロールするを作成する
-    const nodes = loadBrowseNode();
-    const crawlingObjects = nodes.filter(node => node.children.length == 0).map(node => {
-        return {
-            site: "Amazon",
-            page: {
-                kind: "BrowseNode",
-                nodeId: node.nodeId,
-                page: 1
-            }
-        } as CrawlingObject;
-    });
-
-    const batchs = arraySplit(crawlingObjects, 200);
-    for(let batch of batchs) {
-        await crawlingRep.createTasks(batch);
-        console.log(`createTasks: ${batch.length}`)
-    }
 
     // クロールする
     while(true) {
---
+++ lib/src/entries/InitCrawler.ts
@@ -0,0 +1,37 @@
+import { promise } from "selenium-webdriver";
+import { CrawlingObject, indexCrawler } from "../crawlers/IndexCrawler";
+import { loadBrowseNode } from "../data/BrowseNodeLoader";
+import * as scraperapi from "../executions/Scraperapi";
+import { CrawlingRepository } from "../repositories/CrawlingRepository";
+import { getRepositories } from "../system/Database";
+import { Document } from "../system/Document";
+import { sleep } from "../Utils";
+
+const arraySplit = <T = object>(array: T[], n: number): T[][] =>
+  array.reduce((acc: T[][], c, i: number) => (i % n ? acc : [...acc, ...[array.slice(i, i + n)]]), []);
+
+async function run() {
+    const reps = await getRepositories();
+    const crawlingRep = reps.crawling;
+
+    // ここで各カテゴリの1ページ目をクロールするを作成する
+    const nodes = loadBrowseNode();
+    const crawlingObjects = nodes.filter(node => node.children.length == 0).map(node => {
+        return {
+            site: "Amazon",
+            page: {
+                kind: "BrowseNode",
+                nodeId: node.nodeId,
+                page: 1
+            }
+        } as CrawlingObject;
+    });
+
+    const batchs = arraySplit(crawlingObjects, 200);
+    for(let batch of batchs) {
+        await crawlingRep.createTasks(batch);
+        console.log(`createTasks: ${batch.length}`)
+    }
+}
+
+run();
